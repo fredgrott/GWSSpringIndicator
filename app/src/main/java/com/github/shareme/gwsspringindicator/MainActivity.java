2d889f509cb71599d47063b5bf70d567290afa68
Fred Grott <fred.grott@gmail.com>
2015-10-13 09:51:49 -0500

    corrections

--- app/src/main/java/com/github/shareme/gwsspringindicator/MainActivity.java
+++ app/src/main/java/com/github/shareme/gwsspringindicator/MainActivity.java
@@ -2,22 +2,21 @@ package com.github.shareme.gwsspringindicator;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.common.collect.Lists;
+import com.github.shareme.gwsspringindicator.library.ScrollerViewPager;
+import com.github.shareme.gwsspringindicator.library.SpringIndicator;
+
 
 import java.util.List;
 
-import github.chenupt.multiplemodel.viewpager.ModelPagerAdapter;
-import github.chenupt.multiplemodel.viewpager.PagerModelManager;
-import github.chenupt.springindicator.SpringIndicator;
-import github.chenupt.springindicator.viewpager.ScrollerViewPager;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     ScrollerViewPager viewPager;
 
@@ -33,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
 
         PagerModelManager manager = new PagerModelManager();
         manager.addCommonFragment(GuideFragment.class, getBgRes(), getTitles());
-        ModelPagerAdapter adapter = new ModelPagerAdapter(getSupportFragmentManager(), manager);
+        ModelPagerAdapter adapter = new ModelPagerAdapter(getSFragmentManager(), manager);
         viewPager.setAdapter(adapter);
         viewPager.fixScrollSpeed();
 
@@ -47,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private List<Integer> getBgRes(){
-        return Lists.newArrayList(R.drawable.bg1, R.drawable.bg2, R.drawable.bg3, R.drawable.bg4);
+        return Lists.newArrayList(R.mipmap.bg1, R.mipmap.bg2, R.mipmap.bg3, R.mipmap.bg4);
     }
 
 
